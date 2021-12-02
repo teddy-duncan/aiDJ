6e93eb3c7443a42f64fae225faa77341beb08066
teddy-duncan <77077101+teddy-duncan@users.noreply.github.com>
2021-12-01 19:05:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+song="";
+song1="";
+function preload()
+{
+	song=loadSound("music.mp3");
+	song1=loadSound("music.mp3");
+}
+function setup(){
+	canvas= createCanvas(600,500);
+	canvas.center();
+	video=createCapture(VIDEO);
+	video.hide();
+} 
+function draw(){
+	image (video,0,0,600,500);
+}
